6c6da975b6d947fe2c29fe0585d4bda92de358be
Hao Zhang <hao@supportiv.com>
2023-09-01 23:36:58 -0400

    add comments

--- run_rrhf_train.sh
+++ run_rrhf_train.sh
@@ -1,6 +1,10 @@
 set -e
 set -x
 
+# step 4, RRHF/train.py
+# input = train data
+# output = RRHF model
+
 bash train.sh \
     ../ninjatech-ai/open_llama_3b_v2_code_alpaca/checkpoint-800 \
     ../ninjatech-ai/open_llama_3b_v2_code_alpaca/checkpoint-800-rrhf \
