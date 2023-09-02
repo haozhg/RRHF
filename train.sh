72eacd71d72d66a94493ef3c6ba2ad7755160803
Hao Zhang <hao@supportiv.com>
2023-09-01 22:43:49 -0400

    update train.sh

--- train.sh
+++ train.sh
@@ -3,10 +3,12 @@ export SAVE_PATH=$2
 export DATA_PATH=$3
 export MASTER_ADDR="localhost"
 export MASTER_PORT="22"
-export WANDB_DISABLED=true
-wandb offline
 
-python3 -m torch.distributed.launch --master_addr ${MASTER_ADDR} --master_port ${MASTER_PORT} --nproc_per_node=8 --use_env train.py \
+time python3 -m torch.distributed.launch \
+    --master_addr ${MASTER_ADDR} \
+    --master_port ${MASTER_PORT} \
+    --nproc_per_node=2 \
+    --use_env train.py \
     --model_name_or_path $MODEL_PATH \
     --data_path $DATA_PATH \
     --bf16 True \
@@ -18,7 +20,7 @@ python3 -m torch.distributed.launch --master_addr ${MASTER_ADDR} --master_port $
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 100 \
-    --save_total_limit 40 \
+    --save_total_limit 4 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
@@ -26,4 +28,8 @@ python3 -m torch.distributed.launch --master_addr ${MASTER_ADDR} --master_port $
     --logging_steps 1 \
     --fsdp "full_shard auto_wrap" \
     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
-    --tf32 True --model_max_length 192 --rrhf_weight 1
+    --tf32 True \
+    --model_max_length 512 \
+    --rrhf_weight 1 \
+    --report_to wandb \
+    2>&1 | tee -a train.sh.log
